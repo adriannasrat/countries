a1e9269ab56ad454a1c69bb6c65fc088cc365ab2
Adrian <57892572+adriannasrat@users.noreply.github.com>
2026-08-06 17:55:01 +0200

    Add accessible route error boundary

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,10 +4,12 @@ import AppLayout from "../components/layout/AppLayout";
 import CountryDetails from "../pages/CountryDetails";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import RouteError from "../pages/RouteError";
 
 export const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <RouteError />,
     children: [
       {
         path: "/",
